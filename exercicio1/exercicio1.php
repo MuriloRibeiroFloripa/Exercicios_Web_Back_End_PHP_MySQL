10e168e0e40e24c5bf6dad3d40d4f3a8cffa8546
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-10 14:45:12 -0300

    Fundamentos de Programação - Estruturado para os Exercícios, primeiro programa Soma

--- exercicio1/exercicio1.php
+++ exercicio1/exercicio1.php
@@ -1,19 +1,34 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Exercícios1</title>
 	<link rel="stylesheet" href="">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
 </head>
+
 <body>
+
 	<nav>
       <ul>
-        <li class="collection-item"><a href="index.php">Pagina Principal</a></li>
-        <li class="collection-item"><a href="exercicio1\exercicio1.php">Exercicio1</a></li>
-      </ul>
+        <li class="collection-item"><a href="../index.php">Pagina Principal</a></li>
+        <li class="collection-item"><a href="exercicio1.php">Exercicio1</a></li>
+        <p>Fazer um Programa para somar dois Números.</p>
+      </ul>      
     </nav>
+    
+    <br>
+  
+    <form method="post" action="soma.php">
+    	<label> Informe o Número 1: </label>
+    	<input type="text" name="n1">
+    	<label> Informe o Número 2: </label>
+    	<input type="text" name="n2">
+    	<input type="submit" name="Enviar">
+    </form>
 	
 </body>
+
 </html>
---
+++ exercicio1/soma.php
@@ -0,0 +1,27 @@
+<nav>
+	<ul>
+		<li><a href="../index.php">Voltar a Pagina Principal</a></li>
+        <li><a href="exercicio1.php">Exercicio1</a></li>
+    </ul>
+</nav
+
+<br>
+
+<?php
+
+$n1 = $_POST['n1'];
+$n2 = $_POST['n2'];
+
+$soma = $n1 + $n2;
+
+echo ("O Resultado da Soma = $soma");
+
+?>
+
+
+
+
+
+
+
+
