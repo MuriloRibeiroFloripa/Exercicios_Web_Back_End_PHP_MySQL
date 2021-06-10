245510130a90764e80b362f7e8370a58d1f7e2c8
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-10 13:29:44 -0300

    criação do arquivo index.php

---
+++ exercicio1/exercicio1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Exercícios1</title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+</head>
+<body>
+	<nav>
+      <ul>
+        <li class="collection-item"><a href="index.php">Pagina Principal</a></li>
+        <li class="collection-item"><a href="exercicio1\exercicio1.php">Exercicio1</a></li>
+      </ul>
+    </nav>
+	
+</body>
+</html>
